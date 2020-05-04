9ee3956c82ed67c74b4d7d44919461ea5d28c31e
mtoffl01 <tofflerm@gmail.com>
2020-05-04 17:52:49 -0400

    Updates

---
+++ precache-manifest.782fd09bf24a3021b54dedf47a17431e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8888160f99a551262a3efb1edfb99a68",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "c9a54e27aed1f75a7d3a",
+    "url": "/myPortfolio/static/css/2.3ab4c7df.chunk.css"
+  },
+  {
+    "revision": "11424ed8c7faf23d059a",
+    "url": "/myPortfolio/static/css/main.d83aac08.chunk.css"
+  },
+  {
+    "revision": "c9a54e27aed1f75a7d3a",
+    "url": "/myPortfolio/static/js/2.fd81a352.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myPortfolio/static/js/2.fd81a352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11424ed8c7faf23d059a",
+    "url": "/myPortfolio/static/js/main.5d62a790.chunk.js"
+  },
+  {
+    "revision": "2c5ea7f038f67e074bf1",
+    "url": "/myPortfolio/static/js/runtime-main.263b95fc.js"
+  },
+  {
+    "revision": "4d9dfedd5e0d975992d8145f9af26166",
+    "url": "/myPortfolio/static/media/city-kids.4d9dfedd.png"
+  },
+  {
+    "revision": "346bdba546cf18fe82a79d6802fe83ee",
+    "url": "/myPortfolio/static/media/dumbo.346bdba5.png"
+  },
+  {
+    "revision": "3908ee16c5ca19004997be5309d3a1a1",
+    "url": "/myPortfolio/static/media/grace-shopper.3908ee16.jpeg"
+  },
+  {
+    "revision": "a4d305f7a083108afbdf0d5be21171c1",
+    "url": "/myPortfolio/static/media/me.a4d305f7.jpg"
+  },
+  {
+    "revision": "c35ba00d4399e2c20ddb81535154089e",
+    "url": "/myPortfolio/static/media/tastemakers.c35ba00d.jpeg"
+  },
+  {
+    "revision": "6d1cf76033263b022703268aa9ba90f5",
+    "url": "/myPortfolio/static/media/tufts-logo.6d1cf760.jpg"
+  },
+  {
+    "revision": "52b3ecab33320228ed68a150f88c710a",
+    "url": "/myPortfolio/static/media/tufts-sights.52b3ecab.jpeg"
+  },
+  {
+    "revision": "f409637c823e88807cc28f27dbd47886",
+    "url": "/myPortfolio/static/media/washington-square.f409637c.jpeg"
+  }
+]);
