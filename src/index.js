e7e0ea8852d5aa19f9f1db52e2d5c666b790a319
mtoffl01 <tofflerm@gmail.com>
2020-05-05 13:05:48 -0400

    changed homepage url and removed unnecessary browserrouter in index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './index.css';
 import App from './App';
-import Projects from './components/Projects'
 
 ReactDOM.render(
-  <Router>
-    {/* <React.StrictMode> */}
-      <Route exact path='/'>
-        <App />
-      </Route>
-      <Route path="/projects" component={Projects}/>
-    {/* </React.StrictMode> */}
-  </Router>,
+        <App />,
   document.getElementById('root')
 );
 
