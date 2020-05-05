bdaa1866c24e63063e5bcea492b3045738acea12
mtoffl01 <tofflerm@gmail.com>
2020-05-05 13:36:29 -0400

    updated project description text

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,8 +11,8 @@ const Projects = () => {
         </a>
         <div>
           <h2>TasteMakers</h2>
-          <p className="project-description">Tastemakers is a mobile social media
-            application for sharing image-driven experiences with your friends. As a fullstack developer on this group project, I worked across the stack with a particular focus on building out functionality for the user profile, creating a new post and the explore page. My team used the Node.js Sentiment machine learning model in order to detect positive engagement with particular posts and preferentially render similar content on the user's explore page.
+          <p className="project-description">TasteMakers is a mobile social media
+            application for sharing image-driven experiences with your friends. As a full stack developer on this group project, I worked across the stack with a particular focus on the user profile, uploading posts, and the explore page. My team used the Node.js Sentiment machine learning model in order to detect positive engagement with particular posts and preferentially render similar content on the user's explore page.
           </p>
           <a href="https://github.com/tastemakers-node-feratu/capstone1" rel="noopener noreferrer" target="_blank">
             <FaAngleDoubleRight size={50} className="viewBtn" color="#ffe6e6"/>
@@ -25,7 +25,7 @@ const Projects = () => {
         </a>
         <div>
           <h2>Lux Ducks</h2>
-          <p className="project-description">Lux Ducks is an E-Commerce site for purchasing fancy rubber ducks. As a fullstack developer on this project, I worked across the React/Redux/Express/Node.js/SQL stack with a focus on the cart functionality. I also lead the team on schema design.
+          <p className="project-description">Lux Ducks is an E-Commerce site for purchasing fancy rubber ducks. As a full stack developer on this project, I worked across the React/Redux/Express/Node.js/SQL stack with a focus on the cart functionality. I also lead the team on schema design.
           </p>
           <a href="https://grace-shopper-great-gatsby.herokuapp.com/" rel="noopener noreferrer" target="_blank">
             <FaAngleDoubleRight size={50} className="viewBtn" color="#ffe6e6"/>
@@ -38,7 +38,7 @@ const Projects = () => {
         </a>
         <div>
           <h2>Tufts Sights</h2>
-          <p className="project-description">Tufts Sights allows users to take a virtual tour of Tufts campus in one of seven different languages. For the on-campus user, the application will automatically update its geolocation as the user walks around. For the offsite user, one can freely click around the pinned sights to learn more about campus landmarks. My team of four used HTML/CSS/Javascript/Express/Node.js/MongoDB alongside google maps API to build this application.</p>
+          <p className="project-description">Tufts Sights allows users to take a virtual tour of Tufts campus in one of seven different languages. For the on-campus user, the application will automatically update its geolocation as the user walks around. For the offsite user, one can freely click on the pins to learn more about campus landmarks. My team used HTML/CSS/Javascript/Express/Node.js/MongoDB alongside Google Maps API to build this application.</p>
           <a href="https://github.com/tuftsdev/comp20-s2019-team7" rel="noopener noreferrer" target="_blank">
             <FaAngleDoubleRight size={50} className="viewBtn" color="#ffe6e6"/>
           </a>
