55cee65a3e363af6a3c2ccf95632d6c87f0d0a67
mtoffl01 <tofflerm@gmail.com>
2020-05-05 12:33:08 -0400

    Updates

---
+++ precache-manifest.30adb581c72bc0695c6441a34b9ca8d1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53031d94e48f92143579fe44efe0e02e",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "c9a54e27aed1f75a7d3a",
+    "url": "/myPortfolio/static/css/2.3ab4c7df.chunk.css"
+  },
+  {
+    "revision": "421df12ae9384aac7a8a",
+    "url": "/myPortfolio/static/css/main.d5e3f57d.chunk.css"
+  },
+  {
+    "revision": "c9a54e27aed1f75a7d3a",
+    "url": "/myPortfolio/static/js/2.fd81a352.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myPortfolio/static/js/2.fd81a352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "421df12ae9384aac7a8a",
+    "url": "/myPortfolio/static/js/main.1f72ba46.chunk.js"
+  },
+  {
+    "revision": "2c5ea7f038f67e074bf1",
+    "url": "/myPortfolio/static/js/runtime-main.263b95fc.js"
+  },
+  {
+    "revision": "4d9dfedd5e0d975992d8145f9af26166",
+    "url": "/myPortfolio/static/media/city-kids.4d9dfedd.png"
+  },
+  {
+    "revision": "346bdba546cf18fe82a79d6802fe83ee",
+    "url": "/myPortfolio/static/media/dumbo.346bdba5.png"
+  },
+  {
+    "revision": "3908ee16c5ca19004997be5309d3a1a1",
+    "url": "/myPortfolio/static/media/grace-shopper.3908ee16.jpeg"
+  },
+  {
+    "revision": "a4d305f7a083108afbdf0d5be21171c1",
+    "url": "/myPortfolio/static/media/me.a4d305f7.jpg"
+  },
+  {
+    "revision": "c35ba00d4399e2c20ddb81535154089e",
+    "url": "/myPortfolio/static/media/tastemakers.c35ba00d.jpeg"
+  },
+  {
+    "revision": "6d1cf76033263b022703268aa9ba90f5",
+    "url": "/myPortfolio/static/media/tufts-logo.6d1cf760.jpg"
+  },
+  {
+    "revision": "52b3ecab33320228ed68a150f88c710a",
+    "url": "/myPortfolio/static/media/tufts-sights.52b3ecab.jpeg"
+  },
+  {
+    "revision": "f409637c823e88807cc28f27dbd47886",
+    "url": "/myPortfolio/static/media/washington-square.f409637c.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2d7b34c1695467299ced0d9b4717e91b.js"
+  "/myPortfolio/precache-manifest.30adb581c72bc0695c6441a34b9ca8d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myPortfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
